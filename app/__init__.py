c7f7173afe34dd70c8c7ed1223085eee817d4d0f
Dhiya-cjayakumar <dhiya.cjayakumar@gmail.com>
2025-07-25 16:11:30 +0530

    Remove ignored files that were accidentally committed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .routes import routes  
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'your_secret_key'
+    app.secret_key = os.getenv('SECRET_KEY', 'default_dev_secret_key')
     app.register_blueprint(routes)  
     return app
